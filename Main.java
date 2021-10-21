0dcec023fde18cf628562eea28d6d998f961390a
NumenMirus <michaelbastauni@gmail.com>
2021-10-21 22:08:42 +0200

    added menu and functions

--- Main.java
+++ Main.java
@@ -13,10 +13,39 @@ import javax.swing.JOptionPane;
 public class Main{
     public static void main(String[] args){
         final String filepath = "data.csv";
+        Scanner scanner = new Scanner(System.in);
+        int choice = 1;
 
-        storeArgoments(addArgoment(), filepath);
-        storeArgoments(addArgoment(), filepath);
+        while(choice != 0){
+            System.out.println("Bentornato, seleziona l'opzione:\n\n1) Visualizza ripassi di oggi\n2) Aggiungi nuovo argomento\n3) Elimina argomento\n4) Modifica impostazioni argomento\n5) Stampa calendario\n6) Attiva notifiche Desktop\n\n0) ESCI");
+            choice = scanner.nextInt();
+            scanner.nextLine();
 
+            switch(choice){
+                case 1:
+                    //visualizza ripassi di oggi
+                    break;
+                case 2:
+                    storeArgoments(addArgoment(), filepath);
+                    break;
+                case 3:
+                    //elimina argomento
+                    break;
+                case 4:
+                    //modifica impostazioni argomento
+                    break;
+                case 5:
+                    //stampa calendario
+                    break;
+                case 6:
+                    //attiva notifiche
+                    break;  
+                default:
+                    break;
+            }
+        }
+        
+        scanner.close();
     }
 
     public static void storeArgoments(Argoment arg, String filepath){
@@ -83,8 +112,15 @@ public class Main{
         LocalDate fd = LocalDate.parse(finalDate, formatter);
         a.setFinalDate(fd);
 
-
         scanner.close();
+        return a;
+    }
+
+    public static ArrayList<Argoment> visualizeArguments(LocalDate date){
+        ArrayList<Argoment> a = new ArrayList<Argoment>();
+
+        // code here
+
         return a;
     }
 
