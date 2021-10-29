636c23556bb26b472b7619d3ae1b7d0900fd6d7d
NumenMirus <michaelbastauni@gmail.com>
2021-10-29 14:36:01 +0200

    added button class

---
+++ Button.java
@@ -0,0 +1,31 @@
+import javax.swing.JButton;
+
+public class Button extends JButton{
+    
+    Button(){
+        this.setText("Lorem Ipsum");
+        this.setBounds(0, 0, 50, 50);
+        this.setFocusable(false);
+        this.setVerticalAlignment(JButton.CENTER);
+        this.setHorizontalAlignment(JButton.CENTER);
+        this.setVisible(false);
+    }
+
+    Button(String s, int x, int y, int width, int height){
+        this.setText(s);
+        this.setBounds(x, y, width, height);
+        this.setFocusable(false);
+        this.setVerticalAlignment(JButton.CENTER);
+        this.setHorizontalAlignment(JButton.CENTER);
+        this.setVisible(false);
+    }
+
+    Button(String s, int x, int y, int width, int height, boolean visible){
+        this.setText(s);
+        this.setBounds(x, y, width, height);
+        this.setFocusable(false);
+        this.setVerticalAlignment(JButton.CENTER);
+        this.setHorizontalAlignment(JButton.CENTER);
+        this.setVisible(visible);
+    }
+}
